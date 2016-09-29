d12ce6cf1e999815312a0ef93673b44f960e73bf
Alexey <samolet15@mail.ru>
2016-09-30 00:24:28 +0300

    Level indication

--- app/src/main/java/io/github/vkdisco/SampleActivity.java
+++ app/src/main/java/io/github/vkdisco/SampleActivity.java
@@ -20,39 +20,22 @@ public class SampleActivity extends AppCompatActivity
 
     private int mChannelHandle = 0;
     private TrackProgressUpdateThread mUpdateThread = null;
+    private LevelUpdateThread mLevelUpdateThread = null;
     private boolean mPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
-        initBass();
-        Button btnLoadFile = ((Button) findViewById(R.id.btnLoadFile));
-        if (btnLoadFile != null) {
-            btnLoadFile.setOnClickListener(this);
-        }
-        Button btnPlay = ((Button) findViewById(R.id.btnPlay));
-        if (btnPlay != null) {
-            btnPlay.setOnClickListener(this);
-        }
-        Button btnPause = ((Button) findViewById(R.id.btnPause));
-        if (btnPause != null) {
-            btnPause.setOnClickListener(this);
-        }
-        Button btnStop = ((Button) findViewById(R.id.btnStop));
-        if (btnStop != null) {
-            btnStop.setOnClickListener(this);
-        }
-        SeekBar sbTrackProgress = ((SeekBar) findViewById(R.id.sbTrackProgress));
-        if (sbTrackProgress != null) {
-            sbTrackProgress.setOnSeekBarChangeListener(this);
-        }
+        initBass(); //BASS initialization
+        initViews(); //Views initialization
     }
 
     @Override
     protected void onDestroy() {
-        stopProgressUpdate();
-        BASS.BASS_Free();
+        stopLevelUpdate(); //Stopping all updates
+        stopTrackProgressUpdate();
+        BASS.BASS_Free(); //Freeing BASS
         super.onDestroy();
     }
 
@@ -60,7 +43,7 @@ public class SampleActivity extends AppCompatActivity
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
-                case RC_LOAD_FILE:
+                case RC_LOAD_FILE: //If user chosen file to open
                     if (data != null) {
                         String filename = data.getStringExtra(OpenFileActivity.EXTRA_FILENAME);
                         loadFileByName(filename);
@@ -91,29 +74,35 @@ public class SampleActivity extends AppCompatActivity
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (!fromUser) {
+        if (!fromUser) { //Ignoring changing progress by setProgress(int)
             return;
         }
-        long total = BASS.BASS_ChannelGetLength(mChannelHandle, BASS.BASS_POS_BYTE);
+        long total = BASS.BASS_ChannelGetLength(mChannelHandle, BASS.BASS_POS_BYTE); //Setting up position
         long position = Math.round((1.0 * progress / 100) * total);
         BASS.BASS_ChannelSetPosition(mChannelHandle, position, BASS.BASS_POS_BYTE);
     }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
+        //Stopping thread-updater if manual track position changing
         if (mPlaying) {
-            stopProgressUpdate();
+            stopTrackProgressUpdate();
         }
     }
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         if (mPlaying) {
-            startProgressUpdate();
+            startTrackProgressUpdate();
         }
     }
 
     private void onLoadFile() {
+        onTrackStop();
+        if (mChannelHandle != 0) { //If we opening another track, we need to free already opened
+            //(if it opened)
+            BASS.BASS_StreamFree(mChannelHandle);
+        }
         Intent intent = new Intent(this, OpenFileActivity.class);
         startActivityForResult(intent, RC_LOAD_FILE);
     }
@@ -123,7 +112,9 @@ public class SampleActivity extends AppCompatActivity
             return;
         }
         BASS.BASS_ChannelPlay(mChannelHandle, false);
-        startProgressUpdate();
+        //Track playing started, so starting threads-updaters
+        startLevelUpdate();
+        startTrackProgressUpdate();
         mPlaying = true;
     }
 
@@ -131,7 +122,9 @@ public class SampleActivity extends AppCompatActivity
         if (mChannelHandle == 0) {
             return;
         }
-        stopProgressUpdate();
+        //Track to be paused - stopping threads-updaters
+        stopLevelUpdate();
+        stopTrackProgressUpdate();
         BASS.BASS_ChannelPause(mChannelHandle);
         mPlaying = false;
     }
@@ -140,7 +133,8 @@ public class SampleActivity extends AppCompatActivity
         if (mChannelHandle == 0) {
             return;
         }
-        stopProgressUpdate();
+        stopLevelUpdate();
+        stopTrackProgressUpdate();
         mPlaying = false;
         BASS.BASS_ChannelStop(mChannelHandle);
         BASS.BASS_ChannelSetPosition(mChannelHandle, 0, BASS.BASS_POS_BYTE);
@@ -148,6 +142,7 @@ public class SampleActivity extends AppCompatActivity
 
     private void initBass() {
         if (!BASS.BASS_Init(-1, 44100, 0)) {
+            //Initialization default device (-1) at sample frequency 44100kHz without flags (0)
             Button btnLoadFile = ((Button) findViewById(R.id.btnLoadFile));
             if (btnLoadFile != null) {
                 btnLoadFile.setEnabled(false);
@@ -157,11 +152,34 @@ public class SampleActivity extends AppCompatActivity
         }
     }
 
+    private void initViews() { //Setting handlers to all views
+        Button btnLoadFile = ((Button) findViewById(R.id.btnLoadFile));
+        if (btnLoadFile != null) {
+            btnLoadFile.setOnClickListener(this);
+        }
+        Button btnPlay = ((Button) findViewById(R.id.btnPlay));
+        if (btnPlay != null) {
+            btnPlay.setOnClickListener(this);
+        }
+        Button btnPause = ((Button) findViewById(R.id.btnPause));
+        if (btnPause != null) {
+            btnPause.setOnClickListener(this);
+        }
+        Button btnStop = ((Button) findViewById(R.id.btnStop));
+        if (btnStop != null) {
+            btnStop.setOnClickListener(this);
+        }
+        SeekBar sbTrackProgress = ((SeekBar) findViewById(R.id.sbTrackProgress));
+        if (sbTrackProgress != null) {
+            sbTrackProgress.setOnSeekBarChangeListener(this);
+        }
+    }
+
     private void loadFileByName(String filename) {
         if (filename == null) {
             return;
         }
-        mChannelHandle = BASS.BASS_StreamCreateFile(filename, 0, 0, 0);
+        mChannelHandle = BASS.BASS_StreamCreateFile(filename, 0, 0, 0); //Loading audio file by name
         if (mChannelHandle == 0) {
             Toast.makeText(this, R.string.text_bass_streamcreatefile_fail, Toast.LENGTH_SHORT)
                     .show();
@@ -171,25 +189,25 @@ public class SampleActivity extends AppCompatActivity
         playControlsEnabled(true);
     }
 
-    private void startProgressUpdate() {
+    private void startTrackProgressUpdate() {
         if (mUpdateThread != null) {
             return;
         }
-        mUpdateThread = new TrackProgressUpdateThread();
+        mUpdateThread = new TrackProgressUpdateThread(); //Creating and setting up progress thread-updater
         mUpdateThread.setActivity(this);
+        mUpdateThread.setChannelHandle(mChannelHandle);
         SeekBar sbTrackProgress = ((SeekBar) findViewById(R.id.sbTrackProgress));
         mUpdateThread.setSeekBar(sbTrackProgress);
-        mUpdateThread.setChannelHandle(mChannelHandle);
         mUpdateThread.start();
     }
 
-    private void stopProgressUpdate() {
+    private void stopTrackProgressUpdate() {
         if (mUpdateThread == null) {
             return;
         }
         boolean tryJoinThread = true;
         mUpdateThread.setRunning(false);
-        while (tryJoinThread) {
+        while (tryJoinThread) { //Trying to join to thread-updater
             try {
                 mUpdateThread.join();
                 tryJoinThread = false;
@@ -199,7 +217,37 @@ public class SampleActivity extends AppCompatActivity
         mUpdateThread = null;
     }
 
-    private void playControlsEnabled(boolean enabled) {
+    private void startLevelUpdate() { //All the same, but for level indicators
+        if (mLevelUpdateThread != null) {
+            return;
+        }
+        mLevelUpdateThread = new LevelUpdateThread();
+        mLevelUpdateThread.setActivity(this);
+        mLevelUpdateThread.setChannelHandle(mChannelHandle);
+        ProgressBar pbLeftLevel = ((ProgressBar) findViewById(R.id.pbIndicationLeft));
+        mLevelUpdateThread.setProgressBarLevelLeft(pbLeftLevel);
+        ProgressBar pbRightLevel = ((ProgressBar) findViewById(R.id.pbIndicationRight));
+        mLevelUpdateThread.setProgressBarLevelRight(pbRightLevel);
+        mLevelUpdateThread.start();
+    }
+
+    private void stopLevelUpdate() {
+        if (mLevelUpdateThread == null) {
+            return;
+        }
+        boolean tryJoinThread = true;
+        mLevelUpdateThread.setRunning(false);
+        while (tryJoinThread) {
+            try {
+                mLevelUpdateThread.join();
+                tryJoinThread = false;
+            } catch (InterruptedException ignored) {
+            }
+        }
+        mLevelUpdateThread = null;
+    }
+
+    private void playControlsEnabled(boolean enabled) { //Enabling or disabling player controls
         Button btnPlay = ((Button) findViewById(R.id.btnPlay));
         if (btnPlay != null) {
             btnPlay.setEnabled(enabled);
@@ -214,7 +262,13 @@ public class SampleActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * Track progress need to be updated periodically.
+     * If we will do this in main thread (i.e. UI thread), we'll
+     * freeze UI
+     */
     public static class TrackProgressUpdateThread extends Thread {
+        private static final int UPDATE_DELTA_MS = 50;
         private int mChannelHandle = 0;
         private SeekBar mSBTrackProgress = null;
         private Activity mActivity = null;
@@ -224,19 +278,10 @@ public class SampleActivity extends AppCompatActivity
         public void run() {
             while (mRunning) {
                 if (mChannelHandle != 0) {
-                    long total = BASS.BASS_ChannelGetLength(mChannelHandle, BASS.BASS_POS_BYTE);
-                    long position = BASS.BASS_ChannelGetPosition(mChannelHandle, BASS.BASS_POS_BYTE);
-                    if (position >= total) {
-                        mRunning = false;
-                    }
-                    final int sbProgress = ((int) Math.round((1.0 * position / total) * 100));
-                    if (mActivity != null && mSBTrackProgress != null) {
-                        mActivity.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mSBTrackProgress.setProgress(sbProgress);
-                            }
-                        });
+                    updateProgress();
+                    try {
+                        Thread.sleep(UPDATE_DELTA_MS);
+                    } catch (InterruptedException ignored) {
                     }
                 }
             }
@@ -256,6 +301,101 @@ public class SampleActivity extends AppCompatActivity
 
         public void setSeekBar(SeekBar seekBar) {
             this.mSBTrackProgress = seekBar;
+        }
+
+        private void updateProgress() {
+            if (mActivity == null || mSBTrackProgress == null) {
+                return;
+            }
+            long total = BASS.BASS_ChannelGetLength(mChannelHandle, BASS.BASS_POS_BYTE);
+            long position = BASS.BASS_ChannelGetPosition(mChannelHandle, BASS.BASS_POS_BYTE);
+            if (position >= total) {
+                mRunning = false;
+            }
+            final int sbProgress = ((int) Math.round((1.0 * position / total) * 100));
+            mActivity.runOnUiThread(new Runnable() { //ALL UI-OPERATIONS MUST BE EXECUTED FROM UI-THREAD
+                //UI-OPERATIONS FORBIDDEN IN NON-UI THREADS
+                @Override
+                public void run() {
+                    mSBTrackProgress.setProgress(sbProgress);
+                }
+            });
+        }
+    }
+
+    /**
+     * Same thing for track left and right levels
+     */
+    public static class LevelUpdateThread extends Thread {
+        private static final int UPDATE_DELTA_MS = 75;
+        public static final int MAX_LEVEL = 32768;
+        private int mChannelHandle = 0;
+        private ProgressBar mPBLevelLeft = null;
+        private ProgressBar mPBLevelRight = null;
+        private Activity mActivity = null;
+        private boolean mRunning = true;
+
+        @Override
+        public void run() {
+            while (mRunning) {
+                if (mChannelHandle != 0) {
+                    updateLevels();
+                    try {
+                        Thread.sleep(UPDATE_DELTA_MS);
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }
+        }
+
+        public void setRunning(boolean run) {
+            this.mRunning = run;
+        }
+
+        public void setChannelHandle(int channelHandle) {
+            this.mChannelHandle = channelHandle;
+        }
+
+        public void setActivity(Activity activity) {
+            this.mActivity = activity;
+        }
+
+        public void setProgressBarLevelLeft(ProgressBar progressBar) {
+            this.mPBLevelLeft = progressBar;
+        }
+
+        public void setProgressBarLevelRight(ProgressBar progressBar) {
+            this.mPBLevelRight = progressBar;
+        }
+
+        private void updateLevels() {
+            if (mActivity == null) {
+                return;
+            }
+            int level = BASS.BASS_ChannelGetLevel(mChannelHandle);
+            if (level == -1) {
+                return;
+            }
+            if (mPBLevelLeft != null) {
+                int left = level & 0xFFFF;
+                final int pbLeft = ((int) Math.round((1.0 * left / MAX_LEVEL) * 100));
+                mActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPBLevelLeft.setProgress(pbLeft);
+                    }
+                });
+            }
+            if (mPBLevelRight != null) {
+                int right = (level >> 16) & 0xFFFF;
+                final int pbRight = ((int) Math.round((1.0 * right / MAX_LEVEL) * 100));
+                mActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPBLevelRight.setProgress(pbRight);
+                    }
+                });
+            }
         }
     }
 }
