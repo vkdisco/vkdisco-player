079191ae0b44ebd292c55900b9d55280f54ef158
Alexey <samolet15@mail.ru>
2016-11-16 00:47:25 +0200

    VD-16: PlayerService implements interfaces now

--- app/src/main/java/io/github/vkdisco/service/PlayerService.java
+++ app/src/main/java/io/github/vkdisco/service/PlayerService.java
@@ -5,13 +5,19 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import io.github.vkdisco.player.PlayerState;
+import io.github.vkdisco.player.interfaces.OnPlayerStateChangedListener;
+import io.github.vkdisco.player.interfaces.OnPlaylistChangedListener;
+import io.github.vkdisco.player.interfaces.OnTrackSwitchListener;
+
 /**
  * PlayerService
  * Contains Player, provides it communication with user,
  * foreground and background work
  */
 
-public class PlayerService extends Service {
+public class PlayerService extends Service implements OnTrackSwitchListener,
+        OnPlayerStateChangedListener, OnPlaylistChangedListener {
     // TODO: 16.11.2016 Add Player field
     // TODO: 16.11.2016 Add Playlist field
 
@@ -19,5 +25,20 @@ public class PlayerService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onPlayerStateChanged(PlayerState state) {
+        //...
+    }
+
+    @Override
+    public void onPlaylistChanged() {
+        //...
+    }
+
+    @Override
+    public void onTrackSwitch() {
+        //...
     }
 }
