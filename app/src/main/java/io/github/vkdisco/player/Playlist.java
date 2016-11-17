3bf8e88fb4732d8e531ba061b2a6cbf5f8655430
Alexey <samolet15@mail.ru>
2016-11-17 19:37:19 +0200

    VD-16: Initial Playlist class stub

---
+++ app/src/main/java/io/github/vkdisco/player/Playlist.java
@@ -0,0 +1,31 @@
+package io.github.vkdisco.player;
+
+import io.github.vkdisco.model.Track;
+
+/**
+ * Playlist class
+ * WARNING! !STUB!
+ * !Will be implemented!
+ */
+
+public class Playlist {
+    public boolean hasPreviousTrack() {
+        return true;
+    }
+
+    public Track getPreviousTrack() {
+        return null;
+    }
+
+    public Track getCurrentTrack() {
+        return null;
+    }
+
+    public boolean hasNextTrack() {
+        return true;
+    }
+
+    public Track getNextTrack() {
+        return null;
+    }
+}
