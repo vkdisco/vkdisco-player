0ac404947600ce158155920d8439a8a5d502f1f8
Alexey <samolet15@mail.ru>
2016-12-01 20:58:25 +0200

    VD-21: activity_main.xml -> activity_playlist.xml; updated activity_playlist.xml

--- app/src/main/java/io/github/vkdisco/activity/PlaylistActivity.java
+++ app/src/main/java/io/github/vkdisco/activity/PlaylistActivity.java
@@ -13,6 +13,6 @@ public class PlaylistActivity extends PlayerCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playlist);
     }
 }
